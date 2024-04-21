3fbee771d726436cc7b3581388050ad47a8c8463
Andrew Warren <ajwarren85@gmail.com>
2024-04-21 18:11:32 -0500

    upgrade

---
+++ config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,11 @@
+return {
+  -- add gruvbox
+  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+  -- Configure LazyVim to load gruvbox
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin-frappe",
+    },
+  },
+}
